35d02aae34a58218e03607a76cebf4377d44525b
Tsering Lama <tseriing.boksha@gmail.com>
2025-05-14 17:00:48 +0900

    upto create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,4 +14,20 @@ def new
     @restaurant = Restaurant.new
 end
 
+def create
+    @restaurant = Restaurant.new(strong_params)
+    if @restaurant.save
+        redirect_to restaurants_path(@restaurant)
+    else
+        render "new", status: :unprocessable_entity
+    end
+    
+end
+
+private
+
+def strong_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+end
+
 end
